20c06d155afca72acc8b97da19f6309379ef326c
Stepan Rysavy <srysavy@vccp.com>
2019-01-03 00:17:14 +0100

    update october

--- src/layout/homepage/src/script.js
+++ src/layout/homepage/src/script.js
@@ -3,6 +3,21 @@ export default {
   components: {
   },
 	computed: {
+    listPriority1: function () {
+      var list = this.$store.state.komunal18.filter(item => Number(item.LIST) === 1);
+
+      return list[0];
+    },
+    listPriority2: function () {
+      var list = this.$store.state.komunal18.filter(item => Number(item.LIST) === 2);
+
+      return this.sort(list, "NAZEVZAST");
+    },
+    listPriority3: function () {
+      var list = this.$store.state.komunal18.filter(item => Number(item.LIST) === 3);
+
+      return this.sort(list, "NAZEVZAST");
+    }
 	},
   methods: {
     sort: function (list, by) {
